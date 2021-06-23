72e7d37d13018c0c1d8be71ee1dd0a6f8e77fd5f
Marc Contijoch <mcontijoch@abalit.org>
2021-06-23 19:42:39 +0200

    initial commit

---
+++ tests/RoverTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mcontijoch\Rover\Test;
+
+use PHPUnit\Framework\TestCase;
+
+final class RoverTest extends TestCase
+{
+    /** @test */
+    public function test(): void
+    {
+        self::assertEquals(1, 1);
+    }
+}
